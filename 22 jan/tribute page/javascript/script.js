858233c436474359c402e8e31fe7b61936592e85
Nitin <nitingupta5856@gmail.com>
2022-03-31 22:16:03 +0530

    some

---
+++ 22 jan/tribute page/javascript/script.js
@@ -0,0 +1,32 @@
+// console.log('welcome to js')
+
+// console.table({'a':1, 'b':2})
+// var myName= "nitin"
+// console.log(myName)
+// var myName= "nitin"
+// console.log(myName[0])
+// var myName= "nitin"
+// var name1="gupta"
+// console.log(myName===name1)
+// var myName= "nitin"
+// var name1="gupta"
+// var num1
+// console.log(num1)
+// var myName= "nitin"
+// var name1="gupta"
+// var num1= true
+// console.log(num1)
+
+// var obj={
+//     name:'nitin',
+//     city:'delhi'
+// }
+// console.log(obj)
+// var cars=["toyota", "maruti", "bmw"]
+// var cars= new Array("a","b","c")
+// console.log(cars[0])
+// var para1= document.getElementById("p1")
+// alert("Hello")
+// alert(para1)
+var para1= document.getElementsByTagName("p")
+alert(para1.length)
---
+++ 22 jan/tribute page/script1.js
@@ -0,0 +1,29 @@
+// var hello1=document.getElementById("hello1");
+// var btn=document.getElementById("btn");
+// btn.addEventListener("click",function(){
+// hello1.style.color="red"
+// hello1
+// })
+
+var elements=document.getElementsByClassName("td");
+
+console.log(elements);
+
+btn.addEventListener("click",function(){
+for(let i=0;i<elements.length;i++){
+elements[i].style.color="green" 
+}})
+
+
+
+// You have to change the text to }
+
+let arr= [ 12, 35, 1, 10, 34, 1 ];
+    let n = arr.length;
+    print2largest(arr, n);
+var hello1=document.getElementById("hello1");
+ var btn=document.getElementById("btn");
+ btn.addEventListener("click",function(){
+ hello1.style.color="red"
+ hello1
+})
